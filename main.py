2380f1bde9d001ad9d110786c8cc6a46ee55cceb
siniseong <shinhuiseong07@gmail.com>
2025-04-03 09:56:08 +0900

    chore :: 스웨거 정보 수정

--- main.py
+++ main.py
@@ -8,18 +8,15 @@ from models.schemas import CategoriesResponse, CategoryResponse, ErrorResponse
 
 
 app = FastAPI(
-    title="YouTube Subscriber API",
-    description="유튜브 채널 구독자 수 조회 API",
-    version="1.0.0"
+    title="채널 정보 크롤링 (FastAPI, Selenium)",
+    description="채널 정보 (채널명, 설명, 채널 프로필 URL, 구독자 수)",
+    version="1.0.1"
 )
 
-
-@app.get("/", response_model=dict)
-def read_root():
-    return {"message": "YouTube Subscriber API에 오신 것을 환영합니다!"}
-
-
-@app.get("/categories", response_model=CategoriesResponse, responses={400: {"model": ErrorResponse}})
+@app.get("/categories", 
+         response_model=CategoriesResponse, 
+         responses={400: {"model": ErrorResponse}},
+         summary="카테고리별 넘버 조회")
 def get_categories():
     result = get_all_categories()
     if "error" in result:
@@ -27,10 +24,13 @@ def get_categories():
     return result
 
 
-@app.get("/category/{category_id}", response_model=CategoryResponse, responses={400: {"model": ErrorResponse}})
+@app.get("/category/{category_id}", 
+         response_model=CategoryResponse, 
+         responses={400: {"model": ErrorResponse}},
+         summary="카테고리별 채널 정보 크롤링")
 def get_category_channels(
     category_id: int,
-    max_workers: int = Query(4, gt=0, le=10, description="병렬 처리에 사용할 최대 워커 수")
+    max_workers: int = Query(4, gt=0, le=10, description="사용할 워커 수")
 ):
     result = crawl_channels_by_category(category_id, max_workers)
     if "error" in result:
